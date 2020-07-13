4722d0671618beef6b8dc6df872377a7118f11c3
tomaass <tomas@sorejs.cz>
2020-07-13 15:02:35 +0200

    Change fitlering of requests

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const options = {
     const path = proxyRes.client.parser.outgoing.path;
     if (path.includes('annotations') && path.includes('page_size=15')) {
       counter = counter + 1;
-      if (counter % 2) {
+      if (counter % 4 === 0 || counter % 3 === 0) {
         console.log('canceling response ', next);
         proxyRes.statusCode = 408;
       }
